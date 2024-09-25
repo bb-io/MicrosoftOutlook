7e6ef303a0d78683f998f28c7f871efb7d5f05ed
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-09-25 16:50:18 +0200

    Added recipient filter

---
+++ Apps.MicrosoftOutlook/Webhooks/Inputs/ReceiverInput.cs
@@ -0,0 +1,15 @@
+﻿using Blackbird.Applications.Sdk.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.MicrosoftOutlook.Webhooks.Inputs
+{
+    public class ReceiverInput
+    {
+        [Display("Receiver email")]
+        public string? Email { get; set; }
+    }
+}
