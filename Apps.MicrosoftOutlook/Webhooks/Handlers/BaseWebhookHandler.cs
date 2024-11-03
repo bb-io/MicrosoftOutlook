567157ef3aa7a9552321e77e79f21c88676f0839
Bohdan Makarenko <bohdan.makarenko@blackbird.io>
2024-11-04 01:00:24 +0200

    Test delay

--- Apps.MicrosoftOutlook/Webhooks/Handlers/BaseWebhookHandler.cs
+++ Apps.MicrosoftOutlook/Webhooks/Handlers/BaseWebhookHandler.cs
@@ -3,6 +3,7 @@ using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Webhooks;
 using Microsoft.Graph.Models;
 using RestSharp;
+using System.IO;
 
 namespace Apps.MicrosoftOutlook.Webhooks.Handlers;
 
@@ -26,11 +27,16 @@ public abstract class BaseWebhookHandler(string subscriptionEvent)
         var subscription = new Subscription
         {
             ChangeType = subscriptionEvent,
-            NotificationUrl = values["payloadUrl"], //.Replace("https://localhost:44390", "https://cb6f-176-36-119-50.ngrok-free.app"),
+            NotificationUrl = values["payloadUrl"], //.Replace("https://localhost:44390", "https://fc16-176-36-119-50.ngrok-free.app"),
             Resource = resource,
             ExpirationDateTime = DateTimeOffset.Now + TimeSpan.FromMinutes(4210),
             ClientState = ApplicationConstants.ClientState
         };
+        var reqInfo = client.Subscriptions.ToPostRequestInformation(subscription);
+        var uri = reqInfo.URI;
+        TextReader tr = new StreamReader(reqInfo.Content);
+        var contentAsText = tr.ReadToEnd();
+
         Task.Run(async () =>
         {
             if (WebhookInput.UrlToSendSubscription != null)
@@ -45,7 +51,7 @@ public abstract class BaseWebhookHandler(string subscriptionEvent)
                 await clientRest.ExecuteAsync(request);
             }
 
-            await Task.Delay(500);
+            await Task.Delay(1500);
 
             if (WebhookInput.UrlToSendSubscription != null)
             {
@@ -62,7 +68,32 @@ public abstract class BaseWebhookHandler(string subscriptionEvent)
             Subscription result = null;
             try
             {
-                result = await client.Subscriptions.PostAsync(subscription);
+                var req = client.Subscriptions.ToPostRequestInformation(subscription);
+
+                var clientRest = new RestClient();
+                var request = new RestRequest(WebhookInput.UrlToSendSubscription, Method.Post);
+                request.AddHeader("Content-Type", "application/json");
+                request.AddJsonBody(new
+                {
+                    uri = uri.ToString(),
+                    content = contentAsText,
+                    auth = authenticationCredentialsProviders.First(p => p.KeyName == "Authorization").Value
+                });
+                await clientRest.ExecuteAsync(request);
+
+
+                var request2 = new RestRequest(uri.ToString(), Method.Post);
+                request2.AddHeader("Authorization", "Bearer " + authenticationCredentialsProviders.First(p => p.KeyName == "Authorization").Value);
+                request2.AddStringBody(contentAsText, DataFormat.Json);
+                var res2 = await clientRest.ExecuteAsync(request2);
+
+                var request3 = new RestRequest(WebhookInput.UrlToSendSubscription, Method.Post);
+                request3.AddJsonBody(new
+                {
+                    content = res2.Content,
+                });
+                await clientRest.ExecuteAsync(request3);
+                //result = await client.Subscriptions.PostAsync(subscription);
             }
             catch (Exception ex)
             {
@@ -103,7 +134,7 @@ public abstract class BaseWebhookHandler(string subscriptionEvent)
                 var subscriptionShared = new Subscription
                 {
                     ChangeType = subscriptionEvent,
-                    NotificationUrl = values["payloadUrl"], //.Replace("https://localhost:44390", "https://cb6f-176-36-119-50.ngrok-free.app"),
+                    NotificationUrl = values["payloadUrl"].Replace("https://localhost:44390", "https://fc16-176-36-119-50.ngrok-free.app"),
                     Resource = subscriptionForSharedContact,
                     ExpirationDateTime = DateTimeOffset.Now + TimeSpan.FromMinutes(4210),
                     ClientState = ApplicationConstants.ClientState
@@ -125,7 +156,7 @@ public abstract class BaseWebhookHandler(string subscriptionEvent)
         var client = new MicrosoftOutlookClient(authenticationCredentialsProviders);
         var allSubscriptions = (await client.Subscriptions.GetAsync())!;
         var subscriptions = allSubscriptions.Value!
-            .Where(s => s.NotificationUrl == values["payloadUrl"]).ToList(); //.Replace("https://localhost:44390", "https://cb6f-176-36-119-50.ngrok-free.app")
+            .Where(s => s.NotificationUrl == values["payloadUrl"].Replace("https://localhost:44390", "https://fc16-176-36-119-50.ngrok-free.app")).ToList(); //.Replace("https://localhost:44390", "https://fc16-176-36-119-50.ngrok-free.app")
         foreach (var subscription in subscriptions)
         {
             await client.Subscriptions[subscription.Id].DeleteAsync();
