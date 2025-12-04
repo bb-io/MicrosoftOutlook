d61b0c49e1298030a7407c0f032cbe036f411fc3
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-12-04 10:46:05 +0200

    update MessageDataSourceHandler attribute in input classes

--- Apps.MicrosoftOutlook/Models/Mail/Requests/ForwardMessageRequest.cs
+++ Apps.MicrosoftOutlook/Models/Mail/Requests/ForwardMessageRequest.cs
@@ -1,13 +1,13 @@
 ﻿using Apps.MicrosoftOutlook.DataSourceHandlers;
 using Blackbird.Applications.Sdk.Common;
-using Blackbird.Applications.Sdk.Common.Dynamic;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Models.FileDataSourceItems;
 
 namespace Apps.MicrosoftOutlook.Models.Mail.Requests;
 
 public class ForwardMessageRequest
 {
     [Display("Message ID")]
-    [DataSource(typeof(MessageDataSourceHandler))]
+    [FileDataSource(typeof(MessageDataSourceHandler))]
     public string MessageId { get; set; }
     
     [Display("Recipient emails")]
